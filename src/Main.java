5a5e793c11aba6518e7834f23317c17212f01a6f
zarko-maric <zarko.maric11@gmail.com>
2024-10-27 16:32:07 +0100

    Query files and IndexFiles done.

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,86 @@
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Scanner;
+
+//test path: C:\\Users\\Zarko\\IdeaProjects\\Index Text Files\\Text Files
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Please enter a number to choose an action: \n" +
-                " (1) - Specify indexed files and directories. \n " +
-                " (2) - Query files which contain a given word.\n");
-        String option = System.console().readLine();
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the directory path:");
-        String path = scanner.nextLine();
+        while (true) {
+            System.out.println("\nPlease enter a number to choose an action: \n" +
+                    "(1) - Specify indexed files and directories. \n" +
+                    "(2) - Query files which contain a given word.\n" +
+                    "(3) - Exit\n");
 
-        File folder = new File(path);
-        if (folder.exists() && folder.isDirectory()) {
-            IndexFiles(folder);
-        } else {
-            System.out.println("Invalid directory!");
-        }
+            String option = scanner.nextLine();
 
-        if(option.equals("1")) {
-            IndexFiles(folder);
-        }else if(option.equals("2")) {
-            QueryFiles();
-        }else System.out.println("Please enter a valid option");
+            if (option.equals("3")) {
+                System.out.println("Exiting the program.");
+                break;
+            }
 
-    }
+            System.out.println("Please enter the directory path:");
+            String path = scanner.nextLine();
 
-    static void IndexFiles(final File folder){
-        for(final File file : folder.listFiles()){
-            if(file.isDirectory()){
-                IndexFiles(file);
-            }else {
-                System.out.println(file.getName());
+            File folder = new File(path);
+            if (option.equals("1")) {
+                IndexFiles(folder, 0);
+            } else if (option.equals("2")) {
+                QueryFiles(folder);
+            } else {
+                System.out.println("Please enter a valid option");
             }
         }
     }
 
-    static void QueryFiles(){
 
+    static void IndexFiles(final File folder, int level) {
+        if (folder.exists() && folder.isDirectory()) {
+            String indent = "    ".repeat(level);
+            System.out.println(indent + "Folder: " + folder.getName());
+
+            for (final File file : folder.listFiles()) {
+                if (file.isFile()) {
+                    System.out.println(indent + "    File: " + file.getName());
+                }
+            }
+
+            for (final File file : folder.listFiles()) {
+                if (file.isDirectory()) {
+                    IndexFiles(file, level + 1);
+                }
+            }
+        } else {
+            System.out.println("Invalid directory!");
+        }
+    }
+
+    static void QueryFiles(File folder) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the word to search for: ");
+        String word = scanner.nextLine();
+
+        searchWordInFiles(folder, word);
+    }
+
+    static void searchWordInFiles(File folder, String word) {
+        if (folder.exists() && folder.isDirectory()) {
+            for (File file : folder.listFiles()) {
+                if (file.isDirectory()) {
+                    searchWordInFiles(file, word);
+                } else if (file.isFile()) {
+                    try {
+                        String content = Files.readString(file.toPath());
+                        if (content.contains(word)) {
+                            System.out.println("Word found in: " + file.getPath());
+                        }
+                    } catch (IOException e) {
+                        System.out.println("Could not read file: " + file.getPath());
+                    }
+                }
+            }
+        }
     }
 }
